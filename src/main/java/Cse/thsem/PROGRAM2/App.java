c058c6b52b2c69eadeb43725ba0893f039f54fd2
Kalpitha-123 <kalpitha.22cse@cambridge.edu.in>
2025-06-09 17:39:47 +0530

    Add files via upload

---
+++ src/main/java/Cse/thsem/PROGRAM2/App.java
@@ -0,0 +1,13 @@
+package Cse.thsem.PROGRAM2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
